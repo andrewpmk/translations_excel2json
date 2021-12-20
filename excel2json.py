3af65934f4e4271f572291071e96347df6e1fbf4
Andrew MacKinnon <andrewpmk@gmail.com>
2021-12-19 19:13:41 -0500

    Update to remove hardcoded input file and output directory

--- excel2json.py
+++ excel2json.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
 import os
 import pandas
+from pathlib import Path
 
 # Column containing resource key
 resourcekeycolumn = "Resource key"
@@ -39,6 +41,7 @@ columns = {
 
 output = {}
 
+# Process row of Excel file
 def processrow(index, row):
     resourcekey = row[resourcekeycolumn] # column containing resource keys
     if resourcekey in resourcekeyignore:
@@ -51,12 +54,19 @@ def processrow(index, row):
             value = row[column] # Get value
             if pandas.isna(value): # Ignore nan
                 continue
+            # Add language code to output
             if langcode not in output:
                 output[langcode] = {}
+            # Add resource key to output
             output[langcode][resourcekey] = value
 
 def main():
-    excel = pandas.read_excel("Vaccine Hunters Canada Translations.xlsx")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="Input Excel file")
+    parser.add_argument("output", help="Output directory to place .json files")
+    args = parser.parse_args()
+
+    excel = pandas.read_excel(args.input)
 
     if not resourcekeycolumn in excel:
         raise ValueError("Resource key column not found in Excel file")
@@ -64,15 +74,19 @@ def main():
     for index, row in excel.iterrows():
         processrow(index, row)
 
-    saveoutput()
+    saveoutput(args.output)
 
-def saveoutput():
+def saveoutput(outputdir):
     for langcode in output.keys():
-        if not os.path.exists(langcode):
-            os.makedirs(langcode)
-        f = open(langcode + "/translation.json", "w", encoding="utf-8")
+        # Remove hyphens because zh-CN and zh-TW are stored in zhCN and zhTW respectively
+        langcode_path = str(langcode).replace("-", "")
+        directory = os.path.join(outputdir, langcode_path)
+        # Create directory if it doesn't exist
+        Path(directory).mkdir(parents=True, exist_ok=True)
+        f = open(os.path.join(directory, "translation.json"), "w", encoding="utf-8")
         # Pretty print JSON with 4 character indent
         jsondata = output[langcode]
+        # Save JSON to file
         json.dump(jsondata, f, indent=4, sort_keys=True, ensure_ascii=False)
         f.close()
 
